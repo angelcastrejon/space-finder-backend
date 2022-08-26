8c7749bc976e74ad6c2194560a8cc2cf0a84136b
Angel Castrejon <angel.castrejon@gmail.com>
2022-08-26 15:24:44 -0700

    Update README Add infrastructure and services

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # space-finder-backend
 
 ## Getting Started
+- Init project
 `npm init -y`
 
+- Installing dev dependencies
 `npm i -D aws-cdk aws-cdk-lib constructs ts-node typescript`
---
+++ services/node-lambda/hello.ts
@@ -0,0 +1,12 @@
+import { v4 } from "uuid";
+
+
+
+async function handler(event: any, context: any) {
+    return {
+        statusCode: 200,
+        body: 'Hello from aws lambda!' + v4()
+    }
+}
+
+export { handler }
