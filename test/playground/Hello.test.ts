44e3825490799ba14a081ce8cd85ac9cc8ed19c4
Angel Castrejon <angel.castrejon@gmail.com>
2022-08-30 11:50:25 -0700

    Add file for testing endpoints

---
+++ test/playground/Hello.test.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { handler } from '../../services/SpacesTable/Read';
+
+const event: APIGatewayProxyEvent = {
+    queryStringParameters: {
+        location: 'London'
+    }
+} as any;
+
+const result = handler(event, {} as any).then((apiResult)=>{
+    const items = JSON.parse(apiResult.body);
+    console.log(123)
+});
