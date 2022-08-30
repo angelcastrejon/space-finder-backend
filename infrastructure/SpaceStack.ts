37a8e6762d9addcb271d7a084006436b98dab0cd
Angel Castrejon <angel.castrejon@gmail.com>
2022-08-30 11:50:58 -0700

    Update to infrastructure

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,39 +5,46 @@ import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 
 
 export class SpaceStack extends Stack {
 
     private api = new RestApi(this, 'SpaceApi')
-    private spacesTable = new GenericTable(
-        'SpacesTable',
-        'spaceId',
-        this
-    )
+    
+    private spacesTable = new GenericTable(this, {
+        tableName: 'SpacesTable',
+        primaryKey: 'spaceId',
+        createLambdaPath: 'Create',
+        readLambdaPath: 'Read',
+        secondaryIndexes: ['location']
+    })
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
-
-
-        const helloLambda  = new LambdaFunction(this, 'hello-lambda', {
-            runtime: Runtime.NODEJS_16_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main'
-        })
 
         const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
             runtime: Runtime.NODEJS_16_X,
             entry: (join(__dirname, '..', 'services', 'node-lambda','hello.ts')),
             handler: 'handler'
-        })
+        });
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*')
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
 
         // Hello API lambda integration:
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
+
+        // Spaces API Integrations:
+        const spaceResource = this.api.root.addResource('spaces');
+        spaceResource.addMethod('POST', this.spacesTable.createLambdaIntegration);
+        spaceResource.addMethod('GET', this.spacesTable.readLambdaIntegration);
     }
 
 
